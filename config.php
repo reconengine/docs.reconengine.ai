a41bd07d5afd201ee6aaeed4063b56b195cb2b29
reconengine <zach@reconengine.ai>
2021-03-10 20:43:32 -0600

    testing

--- config.php
+++ config.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Str;
 
 return [
-    'baseUrl' => 'https://docs.reconengine.ai :)',
+    'baseUrl' => 'https://docs.reconengine.ai',
     'production' => false,
-    'siteName' => 'Recon - World Class Recommendation Engine for Laravel',
+    'siteName' => 'Recon - World Class Recommendation Engine for Laravel :)',
     'siteDescription' => 'Recon is a recommendation engine as a service with an intuitive API. Built on the same foundation powering Amazon.com',
 
     // Algolia DocSearch credentials
